177383d7cc5944e0bd0eaa91885dc2c3a0f725f0
Xebeth <xebeth@north-edge.com>
2020-01-11 16:44:46 +0100

    Updated the assembly info and added the manifest generation target (bumped the version to 1.0.2

--- ArtifactSystemFixed/Properties/AssemblyInfo.cs
+++ ArtifactSystemFixed/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ArtifactSystemFixed")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Artifact System Fixed Mod for Stardew Valley")]
+[assembly: AssemblyDescription("A fairer artifact system")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ArtifactSystemFixed")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Ilyaki")]
+[assembly: AssemblyProduct("Artifact System Fixed")]
+[assembly: AssemblyCopyright("Copyright © Ilyaki 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
